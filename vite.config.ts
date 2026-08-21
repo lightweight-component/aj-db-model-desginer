b17c9003edf4d0bace53c1a4a291943caa37ab9b
Frank Cheung <frank@ajaxjs.com>
2026-08-21 13:45:10 +0800

    ci

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
+  base: "./",
   plugins: [vue()],
   optimizeDeps: {
     include: ["popper.js", "view-ui-plus/src/components/select/dropdown.vue > popper.js"],
